2dd586672859eb349c8ff34aba40e211eead880e
None <none@none>
2020-11-15 05:00:53 +0000

    cleared out data

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
-name = "52chan"
-url = "http://boards.4x13.net"
+name = "multich"
+url = "http://localhost"
 log = "./ips.txt"
-_port = 5000
+_port = 5250
 archive = "./archive/"
 
 friends = {
     "11chan": "http://xn--uxea.net",
     "0chan": "http://0chan.vip",
+    "52chan": "http://boards.4x13.net",
     "local": url
     }
 
