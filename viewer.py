cee4117de6cd401414a1cb7bbf52518abe9d965c
null <null@null>
2020-12-12 05:25:00 +0000

    fixed final greentext bug

--- viewer.py
+++ viewer.py
@@ -136,8 +136,7 @@ def view_t(board, thread):
         p[3] = p[3].split("<br>")
         print(p[3])
         p[3] = "<br>".join([f"<b class='quote'>{x}</b>"
-                          if x[0] == ">" else x for x in p[3]
-                            if len(x)])
+                          if len(x) and x[0] == ">" else x for x in p[3]])
         p[1] = u.unix2hum(p[1])
         p[3] = p[3].replace("&amp;", "&")
         p = postt.format(*p)
