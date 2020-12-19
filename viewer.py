dc2662e53e3e480502f729cf0c2a90423a038897
null <null@null>
2020-12-19 09:33:31 +0000

    >>reply

--- viewer.py
+++ viewer.py
@@ -180,8 +180,8 @@ def view_t(board, thread):
             p[3] = p[3].replace(r, rep)
 
         if re.compile(r'>>[\d]').search(p[3]):
-            p[3] = re.sub(r'>>([\d\,]+)([\s>]?)',
-                          r'<a href="#\1">&gt;&gt;\1</a>\2',
+            p[3] = re.sub(r'>>([\d\,]+)([\s]?)<',
+                          r'<a href="#\1">&gt;&gt;\1</a><',
                           p[3])
 
         p = postt.format(*p)
