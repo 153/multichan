97e16fc524901ee0fe15cf2a52e84c500d381f95
null <null@null>
2021-04-30 03:03:58 +0000

    more tweaks

--- settings.py
+++ settings.py
@@ -8,10 +8,6 @@ wlist = "./ips.txt"
 bans = "./bans.txt"
 delete = "./delete.txt"
 
-#changeme
-#sha256 of password
-phash = "057ba03d6c44104863dc7361fe4578965d1887360f90a0895882e58a6248fc86"
-
 tags = ["random",  "nsfw"]
 _short = 120
 _long = 10000
@@ -20,7 +16,13 @@ _long = 10000
 refreshtime = 60*15 # Check friend boards every 15 minutes
 friends = {
     "0chan": "http://0chan.vip",
+    "52chan": "http://bbs.4x13.net",
+    "ripirc": "http://multi.404.mn",
+    "kuzlol": "http://multich.kuz.lol",
     "local": url
     }
 
-
+# password hash : changeme
+# sha256 hash of input
+phash = "057ba03d6c44104863dc7361fe4578965d1887360f90a0895882e58a6248fc86"
+# not used yet! 
