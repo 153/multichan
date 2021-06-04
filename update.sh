2983bbf1b290c75228a69fa7a0232ab0437ef3ec
null <null@null>
2021-06-04 08:09:45 +0000

    new easy-update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# easy upgrade
+wget https://raw.githubusercontent.com/153/multichan/master/app.py
+wget https://raw.githubusercontent.com/153/multichan/master/home.py
+wget https://raw.githubusercontent.com/153/multichan/master/daemon.py
+wget https://raw.githubusercontent.com/153/multichan/master/atom.py
+wget https://raw.githubusercontent.com/153/multichan/master/backup.py
+wget https://raw.githubusercontent.com/153/multichan/master/refresh.py
+wget https://raw.githubusercontent.com/153/multichan/master/viewer.py
+wget https://raw.githubusercontent.com/153/multichan/master/writer.py
+wget https://raw.githubusercontent.com/153/multichan/master/whitelist.py
+wget https://raw.githubusercontent.com/153/multichan/master/boards.py
