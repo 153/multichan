666a81c24c30f466cebeda74bfc2d12251444b41
null <null@null>
2021-06-04 08:21:52 +0000

    oops, read 'man wget' lol

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
-# easy upgrade
-wget https://raw.githubusercontent.com/153/multichan/master/app.py
-wget https://raw.githubusercontent.com/153/multichan/master/home.py
-wget https://raw.githubusercontent.com/153/multichan/master/daemon.py
-wget https://raw.githubusercontent.com/153/multichan/master/atom.py
-wget https://raw.githubusercontent.com/153/multichan/master/backup.py
-wget https://raw.githubusercontent.com/153/multichan/master/refresh.py
-wget https://raw.githubusercontent.com/153/multichan/master/viewer.py
-wget https://raw.githubusercontent.com/153/multichan/master/writer.py
-wget https://raw.githubusercontent.com/153/multichan/master/whitelist.py
-wget https://raw.githubusercontent.com/153/multichan/master/boards.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/app.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/home.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/daemon.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/atom.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/backup.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/refresh.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/viewer.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/writer.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/whitelist.py
+wget -N https://raw.githubusercontent.com/153/multichan/master/boards.py
